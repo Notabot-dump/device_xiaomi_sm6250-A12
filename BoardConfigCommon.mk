70b2500da216d2229aa8d9598ad7d39e77b549e4
Arjun-Ingole <arjuningole38@gmail.com>
2021-10-13 13:52:20 +0530

    sm6250-common: Add Build broken hacks
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,8 +6,6 @@
 
 COMMON_PATH := device/xiaomi/sm6250-common
 
-BUILD_BROKEN_DUP_RULES := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
@@ -72,6 +70,11 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+
+# Hacks
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_DUP_RULES := true
 
 # Init
 TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_atoll
